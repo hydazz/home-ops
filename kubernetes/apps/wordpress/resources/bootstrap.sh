19de4ae568a4af20bd8eeac0afca09823142167c
hydazz <alexanderhyde@icloud.com>
2025-08-26 19:24:24 +1000

    chore: rsync does not like times appearently

--- kubernetes/apps/wordpress/resources/bootstrap.sh
+++ kubernetes/apps/wordpress/resources/bootstrap.sh
@@ -8,7 +8,7 @@ WP_SOURCE="/usr/src/wordpress"
 
 echo "Starting WordPress bootstrap..."
 
-rsync -a "${WP_SOURCE}/" "${WP_ROOT}/"
+rsync -rlL --no-perms --no-owner --no-group --no-times "${WP_SOURCE}/" "${WP_ROOT}/"
 echo "WordPress files copied successfully."
 
 # some cool stuff with wp-cli could be done here
