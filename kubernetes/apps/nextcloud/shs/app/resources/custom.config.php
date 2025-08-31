5e968d900fe920228e983b86344a28c05559132d
hydazz <alexanderhyde@icloud.com>
2025-08-31 23:16:38 +1000

    chore: hush warnings

--- kubernetes/apps/nextcloud/shs/app/resources/custom.config.php
+++ kubernetes/apps/nextcloud/shs/app/resources/custom.config.php
@@ -5,7 +5,6 @@ $CONFIG = array(
 	'overwritehost' => 'storage.${D_HS}',
 	'check_data_directory_permissions' => false,
 	'default_phone_region' => 'AU',
-	'enable_previews' => false,
 	'trusted_proxies' => array(
 		0 => '127.0.0.1',
 		1 => '10.0.0.0/8',
@@ -14,8 +13,8 @@ $CONFIG = array(
 		0 => '127.0.0.1',
 		1 => 'storage.${D_HS}',
 	),
-	'filesystem_check_changes' => 1,
 	'simpleSignUpLink.shown' => false,
 	'cache_path' => '/cache',
 	'forwarded_for_headers' => array('HTTP_X_FORWARDED_FOR'),
+	'maintenance_window_start' => 100,
 );
